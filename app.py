14a0983ec279c0fde01e8b632402a5ad98234a0b
Aleksei Kaliagin <>
2023-12-12 19:23:31 +0300

    First App

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-# импорт библиотек
-import streamlit as st
 import pandas as pd
-import plotly_express as px
-
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
+import PIL as Image
 
-import pandas as pd
 
-# Загрузка данных
 D_target = pd.read_csv("./datasets/D_target.csv")
 D_clients = pd.read_csv("./datasets/D_clients.csv")
 D_close_loan = pd.read_csv("./datasets/D_close_loan.csv")
@@ -17,6 +15,13 @@ D_loan = pd.read_csv("./datasets/D_loan.csv")
 D_pens = pd.read_csv("./datasets/D_pens.csv")
 D_salary = pd.read_csv("./datasets/D_salary.csv")
 D_work = pd.read_csv("./datasets/D_work.csv")
+
+age_target = Image.open("./datasets/age_target.png")
+corr = Image.open("./datasets/corr.png")
+gender_age = Image.open("./datasets/gender_age.png")
+pairplot = Image.open("./datasets/pairplot.png")
+pens_target = Image.open("./datasets/pens_target.png")
+personal_income = Image.open("./datasets/personal_income.png")
 
 
 merged_data = pd.merge(D_clients, D_target, left_on="ID", right_on="ID_CLIENT", how="left")
@@ -33,7 +38,40 @@ merged_data = pd.merge(merged_data, loans_data, right_on="ID", left_on="ID", how
 merged_data = merged_data[["AGREEMENT_RK", "TARGET", "AGE", "SOCSTATUS_WORK_FL", "SOCSTATUS_PENS_FL",
     "GENDER", "CHILD_TOTAL", "DEPENDANTS", "PERSONAL_INCOME",
     "LOAN_NUM_TOTAL", "LOAN_NUM_CLOSED"]]
+perc_95 = np.nanpercentile(merged_data["PERSONAL_INCOME"], 95)
+merged_data["PERSONAL_INCOME"] = np.where(merged_data["PERSONAL_INCOME"] > perc_95, np.NaN,merged_data["PERSONAL_INCOME"])
+merged_data["GENDER"].replace(0, "Female", inplace=True)
+merged_data["GENDER"].replace(1, "Male", inplace=True)
+merged_data["SOCSTATUS_PENS_FL"].replace(0, "not_pens", inplace=True)
+merged_data["SOCSTATUS_PENS_FL"].replace(1, "pens", inplace=True)
+merged_data["SOCSTATUS_WORK_FL"].replace(0, "not_work", inplace=True)
+merged_data["SOCSTATUS_WORK_FL"].replace(1, "work", inplace=True)
+df_num = merged_data[["AGE","CHILD_TOTAL", "DEPENDANTS", "PERSONAL_INCOME", "LOAN_NUM_TOTAL", "LOAN_NUM_CLOSED"]]
+stat = df_num.describe()
 
 st.title("Streamlit app for EDA of Bank's clients")
+st.divider()
 
-st.write(merged_data)
+st.write('**Распределение по полу и возрасту**')
+st.image(gender_age)
+st.divider()
+
+st.write('**Распределение дохода**')
+st.image(personal_income)
+st.divider()
+
+st.write('**Матрица корреляций**')
+st.image(corr)
+st.divider()
+
+st.write('**Распределение признаков между собой**')
+st.image(pairplot)
+st.divider()
+
+st.write('**Распределение возраста к таргету**')
+st.image(age_target)
+st.divider()
+
+st.write('**Распределение флага пенсионера к таргету**')
+st.image(gender_age)
+st.divider()
